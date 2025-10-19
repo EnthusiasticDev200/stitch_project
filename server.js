9c83d54e403e67bad15e77ce25723f69566bb90a
Ovie <joshuamakanjuola99@gmail.com>
2025-10-19 23:25:20 +0100

    Set-up server file

---
+++ server.js
@@ -0,0 +1,91 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import cors from 'cors'
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import http from 'http'
+
+import allowAccess from './backend/config/cors.js'
+
+const app = express()
+
+const server = http.createServer(app)
+
+
+dotenv.config()
+
+
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+
+
+// CORS set-up
+app.use(cors( 
+    {
+        origin : function(origin, callback){
+            if( !origin || allowAccess.includes(origin)){
+                callback(null, true)
+            }else callback( new Error('Site not allowed by CORS'))
+        },
+        method : ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+        credentials : true
+    }))
+
+// helmet setup
+if (process.env.NODE_ENV === 'production'){
+    app.use(helmet())
+} else{
+    app.use(helmet({
+        contentSecurityPolicy :{
+            useDefaults : true,
+            'script-src' : ['self', 'unsafe-inlie']
+        }
+    }))
+}
+
+
+
+const PORT = process.env.APP_PORT
+
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to Stitch ')
+})
+
+
+
+
+server.listen(PORT, ()=>{
+    console.log(`Server actively running on http://localhost:${PORT}`)
+})
+
+
+
+// Catch invalid route
+app.use((req, res)=>{
+    res.status(404).json({
+        success : false,
+        error : " Route not found"
+    })
+})
+
+
+//Error handling
+app.use((err, req, res, next)=>{
+    res.status(500).json({
+        success : false,
+        error : " Internal server error"
+    })
+})
+
+
+
+
+
+
+
+
+
+
