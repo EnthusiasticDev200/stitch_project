9094baea39b3b6750ab787fb27bb3c4a0465061f
Ovie <joshuamakanjuola99@gmail.com>
2025-11-08 15:45:08 +0100

    Created email verification

--- utils/mailer.js
+++ utils/mailer.js
@@ -27,4 +27,20 @@ const sendOtpEmail = async (toEmail, otp) => {
   
 };
 
-export default sendOtpEmail ;
+const emailVerification = async (toEmail, otp)=>{
+  try{
+      const msg = await mg.messages.create(process.env.MAILGUN_DOMAIN,{
+      from: `"Stitch Support" <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+      to: [toEmail],
+      subject: "Email Verification",
+      html: `<h4> Dear User, to verify your email, please click on the below link </h4> <br> 
+            <h3> <a style="colour:blue" href="http://localhost:3310/v1/auth/verifyemail/${toEmail}/${otp}">Verify Email Link</a> </h3>
+            <h4> <p>It expires in 10 minutes.</p>  </h4> `
+  })
+  }catch(err){
+    console.log("Error verifying email", err)
+    throw err
+  }
+}
+
+export  { sendOtpEmail, emailVerification} ;
